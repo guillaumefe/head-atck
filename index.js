dc9278712969dfe62ab9cf081a2b17acbb4eb456
Guillaume Ferron <guillaume.ferron@gmail.com>
2021-02-07 22:55:55 +0100

    clean up

--- index.js
+++ index.js
@@ -2,20 +2,11 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 
-// https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
 let file = __filename;
-let hdrs = {'X-Custom-Header': 'there is an opportunity'};
+let hdrs = {'X-Custom-Header': 'alert("you have been hacked")'};
 
 app.head('/', (req,res) => {
-    console.log('Head request received', uuidv4())
-    res.sendFile(file, { headers: hdrs, lastModified: false, etag: false });
+    res.sendFile(file, { headers: hdrs }); // maybe something here
 })
 
 app.get('/', (req,res) => {
